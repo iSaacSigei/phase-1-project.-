177412c6d6535ae1bf574837d4eacda10e438b32
iSaacSigei <sigei.isack@student.moringaschool.com>
2022-09-01 01:10:41 +0300

    restructuring JS functions

--- JS/main.js
+++ JS/main.js
@@ -1,20 +1,14 @@
 const initialize = () => {
     const inputCity = document.getElementById('search_hotel')
-  
+
     inputCity.addEventListener('submit', (e) => {
-      e.preventDefault();
-      const input = document.getElementById('city');
-  
-      fetch(`http://localhost:3000/${input.value}`)
-      .then(response => response.json())
-    .then(data => {
-        const hotel = document.getElementByClassName('dataFetch');
-        const summary = document.querySelector('section#movieDetails p');
-  
-        title.innerText = data.title;
-        summary.innerText = data.summary;
-      });
+        e.preventDefault();
+        const input = document.getElementById('city');
+
+        fetch(`http://localhost:3000/${input.value}`)
+            .then(response => response.json())
+            .then(data => analyseData(data));
     })
 }
-    
-  document.addEventListener('DOMContentLoaded', initialize);
+
+document.addEventListener('DOMContentLoaded', initialize);
